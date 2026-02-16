9776b05955dfcf8d000f67d3fd13a1023d8d32f6
ARIVSAA <164269824+ARIVSAA@users.noreply.github.com>
2026-02-16 13:03:53 +0800

    Add profile page

---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+
+      <p>Name: Arvinsaikhan</p>
+      <p>ID: УЛ05212356</p>
+      <p>Email: ari1111@gmail.com</p>
+    </div>
+  );
+}
